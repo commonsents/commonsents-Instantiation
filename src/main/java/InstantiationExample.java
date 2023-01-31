74cee6ac8bcd68bd5022dce5a60d9dd78492e0d6
Karston Kelly <bookelly11@gmail.com>
2023-01-31 23:30:50 +0000

    created object

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object hiya = new Object();
+        return hiya;
     }
 }
